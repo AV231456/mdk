977a1d504a3104f0ce20e87fe72f02832333d4ee
AV231456 <vkajmov883@gmail.com>
2024-09-10 10:52:55 +0400

    Add files via upload

---
+++ AV/WinFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Image.FromFile(@"C:\Users\Student\Downloads\fizio.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Image.FromFile(@"C:\Users\Student\Downloads\Bit.png");
+        }
+    }
+}
